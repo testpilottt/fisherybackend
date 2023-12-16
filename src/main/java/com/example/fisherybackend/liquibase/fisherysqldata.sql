c27c53f2d043c0ae71f2ee2a809ed4a4fedb6495
Rohan Aubeeluck <rohan.aubeeluck@gmail.com>
2023-12-16 19:31:58 +0400

    Threshold system

--- src/main/java/com/example/fisherybackend/liquibase/fisherysqldata.sql
+++ src/main/java/com/example/fisherybackend/liquibase/fisherysqldata.sql
@@ -2,5 +2,5 @@ INSERT INTO `members` (`member_id`, `first_name`, `hours_logged`, `last_name`, `
 VALUES ('1', 'Rohan', NULL, 'Aubeeluck', 'password', NULL, 'rohan', 'ADMIN'),
        ('2', 'Diksha', NULL, 'Aubeeluck', 'password', NULL, 'diksha',  'ADMIN');
 
-INSERT INTO `country_setting` (`country_setting_id`, `country`, `latitude`, `longtitude`) VALUES ('1', 'MAURITIUS', '-20.348404', '57.552152');
-
+INSERT INTO `country_setting` (`country_setting_id`, `thresh_hold_north_east`, `thresh_hold_north_west`, `thresh_hold_south_east`, `thresh_hold_south_west`, `country`, `latitude`, `longtitude`)
+VALUES ('1', '90', '90', '80', '70', 'MAURITIUS', '-20.348404', '57.552152');
